0238394070c207c5f79a75283e2c3899ab93798e
Аяз <112683817+Minnegaleev-Ayaz@users.noreply.github.com>
2023-10-16 19:44:09 +0500

    Revert "Home work1"

--- app/src/main/java/com/example/androifgh/utils/ParamsKey.kt
+++
@@ -1,9 +0,0 @@
-package com.example.androifgh.utils
-
-object ParamsKey {
-    const val TITLE_KEY = "TITLE_KEY"
-    const val MESSAGE_KEY = "MESSAGE_KEY"
-    const val FIRST_FRAGMENT_TAG = "FIRST_FRAGMENT_TAG"
-    const val SECOND_FRAGMENT_TAG = "SECOND_FRAGMENT_TAG"
-    const val THIRD_FRAGMENT_TAG = "THIRD_FRAGMENT_TAG"
-}
